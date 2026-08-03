7e0bec6c0cb93e237548625ad679efd1303868d9
SousanIsabella <isabellasousa2422@gmail.com>
2026-08-03 00:01:34 -0300

    adicionei preceptores_pacientes_flamenguistas e ultimo_atendimento_pacientes (etapa 2)

--- frontend/backend/consultas_avancadas.py
+++ frontend/backend/consultas_avancadas.py
@@ -20,4 +20,41 @@ def preceptores_pacientes_flamenguistas(session: Session):
         
     return resultados
 
+def ultimo_atendimento_pacientes(session: Session):
+    #subconsulta para encontrar a data mais recente de cada paciente
+    subquery = session.query(
+        Atendimento.id_paciente,
+        func.max(Atendimento.data_hora).label("última_data")
+    ).group_by(Atendimento.id_paciente).subquery()
+
+    #apelidos para a tabela Pessoa
+    PessoaPaciente = aliased(Pessoa)
+    PessoaResidente = aliased(Pessoa)
+    PessoaPreceptor = aliased(Pessoa)
+
+    resultados = session.query(
+        PessoaPaciente.nome.label('nome_paciente'),
+        Atendimento.data_hora,
+        PessoaResidente.nome.label('nome_residente'),
+        PessoaPreceptor.nome.label('nome_preceptor'),
+        func.string_agg(Procedimento.nome, ', ').label('procedimentos')
+    ).\
+    join(subquery, (Atendimento.id_paciente == subquery.c.id_paciente) & (Atendimento.data_hora == subquery.c.ultima_data)).\
+    join(Paciente, Paciente.id_pessoa == Atendimento.id_paciente).\
+    join(PessoaPaciente, PessoaPaciente.id_pessoa == Paciente.id_pessoa).\
+    join(Residente, Residente.id_profissional == Atendimento.id_residente).\
+    join(PessoaResidente, PessoaResidente.id_pessoa == Residente.id_profissional).\
+    join(Preceptor, Preceptor.id_profissional == Atendimento.id_preceptor).\
+    join(PessoaPreceptor, PessoaPreceptor.id_pessoa == Preceptor.id_profissional).\
+    outerjoin(ProcedimentoRealizado, ProcedimentoRealizado.id_atendimento == Atendimento.id_atendimento).\
+    outerjoin(Procedimento, Procedimento.id_procedimento == ProcedimentoRealizado.id_procedimento).\
+    group_by(
+        PessoaPaciente.nome,
+        Atendimento.data_hora,
+        PessoaResidente.nome,
+        PessoaPreceptor.nome
+    ).all()
+
+    return resultados
+    
 
